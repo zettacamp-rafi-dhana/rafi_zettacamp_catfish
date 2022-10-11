9989ba578316d04295833f338b7d8d407e8b3291
Rafi Dhana <rafi.dhana@zettacamp.pro>
2022-10-11 15:39:05 +0700

    Revert "update js"
    This reverts commit 2fb67c857fa7c7ec7015d208996c6fc054b8f921.

--- main.js
+++ main.js
@@ -4,27 +4,26 @@ const ppn = 10;
 let afDiskon;
 let afPpn;
 
-function purchase (price, diskon, ppn) {
-
-    diskon = price * diskon / 100;
-    afDiskon = price - diskon;
-    ppn = price * ppn / 100;
-    afPpn = afDiskon + ppn;
-
-    console.log("Harga Buku Rp. " + price);
-    console.log("Diskon Rp. " + diskon);
-    console.log("Harga setelah diskon Rp. " + afDiskon);
-    console.log("Total pajak Rp. " + ppn);
-    console.log("Total Harga Rp. " + afPpn);
-
-
-}
 
 
 let beli = confirm("Apakah anda ingin membeli buku? (y/n)");
 
 if (beli == true ) {
+    function purchase (price, diskon, ppn) {
+
+        diskon = price * diskon / 100;
+        afDiskon = price - diskon;
+        ppn = price * ppn / 100;
+        afPpn = afDiskon + ppn;
     
+        console.log("Harga Buku Rp. " + price);
+        console.log("Diskon Rp. " + diskon);
+        console.log("Harga setelah diskon Rp. " + afDiskon);
+        console.log("Total pajak Rp. " + ppn);
+        console.log("Total Harga Rp. " + afPpn);
+    
+    
+    }
     purchase(100000,20,ppn);
     
 } else {
