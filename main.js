c8ef0e1d3a59a80ba71666bccdc4e2c7cf74d7b8
Rafi Dhana <rafi.dhana@zettacamp.pro>
2022-10-11 15:49:20 +0700

    update

--- main.js
+++ main.js
@@ -1,66 +1,66 @@
-// let price;
-// let diskon;
-// const ppn = 10;
-// let afDiskon;
-// let afPpn;
+let price;
+let diskon;
+const ppn = 10;
+let afDiskon;
+let afPpn;
 
-// function purchase (price, diskon, ppn) {
+function purchase (price, diskon, ppn) {
 
-//     diskon = price * diskon / 100;
-//     afDiskon = price - diskon;
-//     ppn = price * ppn / 100;
-//     afPpn = afDiskon + ppn;
+    diskon = price * diskon / 100;
+    afDiskon = price - diskon;
+    ppn = price * ppn / 100;
+    afPpn = afDiskon + ppn;
 
-//     console.log("Harga Buku Rp. " + price);
-//     console.log("Diskon Rp. " + diskon);
-//     console.log("Harga setelah diskon Rp. " + afDiskon);
-//     console.log("Total pajak Rp. " + ppn);
-//     console.log("Total Harga Rp. " + afPpn);
-// }
+    console.log("Harga Buku Rp. " + price);
+    console.log("Diskon Rp. " + diskon);
+    console.log("Harga setelah diskon Rp. " + afDiskon);
+    console.log("Total pajak Rp. " + ppn);
+    console.log("Total Harga Rp. " + afPpn);
+}
 
 
-// let beli = confirm("Apakah anda ingin membeli buku? (y/n)");
+let beli = confirm("Apakah anda ingin membeli buku? (y/n)");
 
-// if (beli == true ) {
+if (beli == true ) {
     
-//     purchase(100000,20,ppn);
+    purchase(100000,20,ppn);
     
-// } else {
-    
-//  console.log("sampai berjumpa lagi");
-// }
-
-
-
-let inputBooks;
-let stocks = 10;
-const disc = 50 / 100;
-let priceBook = 10000;
-let totalPrice;
-const tax = 10 / 100;
-
-function bookPurchase() {
-let wantToBuy = confirm('Want to buy in my store?');
-inputBooks = prompt("Enter the number of books you want to buy : " + stocks);
-for (inputBooks; inputBooks <= stocks; inputBooks) {
-if (stocks > 0 && inputBooks <= stocks) {
-totalPrice = priceBook * inputBooks;
-totalPrice = totalPrice - totalPrice * disc;
-totalPrice = totalPrice + totalPrice * tax;
-console.log("You buy" + inputBookss+ "Books");
-console.log("Total price :" + totalPrice);
-stocks = stocks - inputBooks;
-inputBooks = prompt("Enter the number of books you want to buy :" + stocks);
-} else if (stocks > 0 && inputBooks > stocks) {
-console.log("You can not buy more than stocks");
-console.log(stocks+ "books left");
 } else {
-console.log('stock is running out');
-break;
-}
-}
+    
+ console.log("sampai berjumpa lagi");
 }
 
+
+
+// let inputBooks;
+// let stocks = 5;
+// const disc = 50 / 100;
+// let priceBook = 10000;
+// let totalPrice;
+// const tax = 10 / 100;
+
+// function bookPurchase() {
+// let wantToBuy = confirm('Apakah anda ingin membeli buku?');
+// inputBooks = prompt("Enter the number of books you want to buy : " + stocks);
+// for (inputBooks; inputBooks <= stocks; inputBooks) {
+// if (stocks > 0 && inputBooks <= stocks) {
+// totalPrice = priceBook * inputBooks;
+// totalPrice = totalPrice - totalPrice * disc;
+// totalPrice = totalPrice + totalPrice * tax;
+// console.log("You buy" + inputBooks+ "Books");
+// console.log("Total price :" + totalPrice);
+// stocks = stocks - inputBooks;
+// inputBooks = prompt("Enter the number of books you want to buy :" + stocks);
+// } else if (stocks > 0 && inputBooks > stocks) {
+// console.log("You can not buy more than stocks");
+// console.log(stocks+ "books left");
+// } else {
+// console.log('stock is running out');
+// break;
+// }
+// }
+// }
+//  bookPurchase();
 
 
 
