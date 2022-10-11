be6a42d242e11703ef3b21cb96a35ffdc3722a88
Rafi Dhana <rafi.dhana@zettacamp.pro>
2022-10-11 16:26:34 +0700

    update js

--- main.js
+++ main.js
@@ -1,30 +1,30 @@
-let price;
-let diskon;
+let price = 100000;
+let diskon = 15;
 const ppn = 10;
 let afDiskon;
 let afPpn;
 
+function purchase (price, diskon, ppn) {
 
+    diskon = price * diskon / 100;
+    afDiskon = price - diskon;
+    ppn = price * ppn / 100;
+    afPpn = afDiskon + ppn;
+
+    console.log("Harga Buku Rp. " + price);
+    console.log("Diskon Rp. " + diskon);
+    console.log("Harga setelah diskon Rp. " + afDiskon);
+    console.log("Total pajak Rp. " + ppn);
+    console.log("Total Harga Rp. " + afPpn);
+
+
+}
 
 let beli = confirm("Apakah anda ingin membeli buku? (y/n)");
 
 if (beli == true ) {
-    function purchase (price, diskon, ppn) {
-
-        diskon = price * diskon / 100;
-        afDiskon = price - diskon;
-        ppn = price * ppn / 100;
-        afPpn = afDiskon + ppn;
-    
-        console.log("Harga Buku Rp. " + price);
-        console.log("Diskon Rp. " + diskon);
-        console.log("Harga setelah diskon Rp. " + afDiskon);
-        console.log("Total pajak Rp. " + ppn);
-        console.log("Total Harga Rp. " + afPpn);
-    
-    
-    }
-    purchase(100000,20,ppn);
+   
+    purchase(price,diskon,ppn);
     
 } else {
     
