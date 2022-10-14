8c85a25df0f4b5ab5b162ec40841cf2e9f366555
Rafi Dhana <rafi.dhana@zettacamp.pro>
2022-10-14 16:37:11 +0700

    clearer  return type

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var num1 = 9;
 var num2 = 19;
 function slice(txt, num1, num2) {
     var sliced = txt.slice(num1, num2);
-    console.log(sliced);
+    return sliced;
 }
-slice(txt, num1, num2);
+console.log(slice(txt, num1, num2));
--- index.ts
+++ index.ts
@@ -1,13 +1,13 @@
 console.log("hello world")
 
-let txt = "Learning Typescript is different than Javascript";
+    let txt = "Learning Typescript is different than Javascript";
     const num1 = 9;
     const num2 = 19;
 
-function slice(txt: string, num1:number, num2:number) {
+function slice(txt:string , num1:number, num2:number): string {
     
     let sliced = txt.slice(num1, num2);
-    console.log(sliced);
+    return sliced;
 }
 
-slice(txt, num1, num2);
+console.log(slice(txt, num1, num2));
